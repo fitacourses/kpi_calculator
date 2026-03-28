030cd7726da1504d2c543a2afdbf049b71c87b7c
sld-devops <sandisfifteen@gmail.com>
2026-03-28 21:15:23 +0200

    Fix pace calculation from MM:SS and restructure dataset with time and elevation columns

--- runners_weekly_analyzer/main.py
+++ runners_weekly_analyzer/main.py
@@ -5,12 +5,15 @@ df = pd.read_csv("data.csv")
 
 # TODO-DONE: group by runner and calculate total distance, average pace, average heart rate, total elevation
 # hint: df.groupby("runner")["kolonna"].sum() / .mean()
-kpi = (df.groupby("runner")[["distance", "pace", "bpm", "elevation"]].agg({"distance": "sum", "elevation": "sum","pace": "mean", "bpm": "mean"}))
+kpi = (df.groupby("runner")[["distance", "time", "elevation", "bpm"]].agg({"distance": "sum", "elevation": "sum", "bpm": "mean"}))
 kpi["bpm"] = kpi["bpm"].round(1) # round bpm to decimal
 print(kpi)
 
-# TODO 3: calculate efficiency score per session
-# formula: (distance * 0.3) + (1/pace * 10 * 0.4) + (elevation/100 * 0.2) - (bpm/1000 * 0.1)
-# hint: df["efficiency"] adds new column during process
+# TODO-DONE: convert time column from MM:SS to decimal minutes (pace)
+# split "time" column by ":" to get minutes and seconds
+parts = df["time"].str.split(":") # split each rows "time" and give list of 2 strings: ["142", "51"]
+# get elements from parts and convert to ints, divide by 60 to get pace in decimal minutes, save in new column "pace"
+df["pace"] = parts.str[0].astype(int) + (parts.str[1].astype(int) / 60)
 
+# TODO-DONE: calculate efficiency score per session
 df["efficiency"] = (df["distance"] * 0.3) + (1/df["pace"] * 10 * 0.4) + (df["elevation"]/100 * 0.2) - (df["bpm"]/1000 * 0.1)
